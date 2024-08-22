260f4a5303756ecc88e2a65ee8f6e9c74f33074c
“Lisalin5813010” <lisalin5813010@yahoo.de>
2024-08-22 21:16:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleCSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 输出到控制台，提示用户输入名字
+            Console.Write("Please enter your name: ");
+            
+            // 从控制台读取用户输入的名字
+            string userName = Console.ReadLine();
+            
+            // 输出问候消息
+            Console.WriteLine("Hello, " + userName + "! Welcome to the simple C# program.");
+            
+            // 等待用户按任意键退出
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
